45cdb633853b7b437176a0f30d2a79a8fc9570e4
Dodi Aditya <dodiaridharma@gmail.com>
2025-01-05 23:20:58 +0800

    handle download model

--- controllers/weather.py
+++ controllers/weather.py
@@ -5,10 +5,36 @@ from flask_restful import Resource
 from PIL import Image
 
 import numpy as np
+import requests
 from tensorflow.keras.models import load_model # type: ignore
 from tensorflow.keras.utils import img_to_array # type: ignore
 
-model = load_model(os.path.join(os.path.dirname(__file__), '../trained_models/weather.h5'))
+from tensorflow.python.lib.io import file_io
+
+def download_model(url, save_path):
+    # Cek apakah model sudah ada
+    if not os.path.exists(save_path):
+        print("Downloading model...")
+        response = requests.get(url, stream=True)
+        response.raise_for_status()  # Raise error jika gagal
+        with open(save_path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        print("Model downloaded successfully!")
+    else:
+        print("Model already exists.")
+
+# URL model (ubah ke link model Anda)
+model_url = "https://drive.google.com/file/d/1GiB7gqNFAeKIQetvGI0h9Gby5q64TaUa/view?usp=sharing"
+model_path = "trained_models/weather.h5"
+
+# Unduh model jika belum ada
+download_model(model_url, model_path)
+
+
+# model = load_model(os.path.join(os.path.dirname(__file__), '../trained_models/weather.h5'))
+model = load_model(os.path.join(os.path.dirname(__file__), "../% s" % model_path))
+
 class_names = ['cloudy', 'rain', 'shine', 'sunrise']
 
 class Weather(Resource):
